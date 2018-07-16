f09f4b442c65b0506aca7f30d8495a40ff7408d6
Faris Tangastani <faris@Fariss-MacBook-Pro.local>
2018-07-16 13:55:40 +0200

    server.get changes

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const db = require(path.resolve( __dirname, "./config/db.js" ))
 const pg = require("pg");
 const WEBPORT = process.env.WEBPORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
+const app = next({ dir: '.', dev });
 const handle = app.getRequestHandler();
 
 require ('dotenv').load();
@@ -16,6 +16,19 @@ app
   .prepare()
   .then(() => {
     const server = express();
+    if (!dev) {
+      // Enforce SSL & HSTS in production
+      server.use(function(req, res, next) {
+        var proto = req.headers["x-forwarded-proto"];
+        if (proto === "https") {
+          res.set({
+            'Strict-Transport-Security': 'max-age=31557600' // one-year
+          });
+          return next();
+        }
+        res.redirect("https://" + req.headers.host + req.url);
+      });
+    }
     server.use(bodyParser.urlencoded({extended:true}))
     server.use(bodyParser.json());
 
@@ -23,14 +36,11 @@ app
       maxAge: dev ? '0' : '365d'
     }));
 
-    server.get('/', (req, res) => {
-      return app.render(req, res, '/index', req.query)
-    })
+      // Example server-side routing
+      server.get('/about', (req, res) => {
+        return nextApp.render(req, res, '/a', req.query)
+      })
 
-    // Example server-side routing
-    server.get('/about', (req, res) => {
-      return app.render(req, res, '/about', req.query)
-    })
     server.post('/user/interest', (req, res) => {
       console.log(req.body);
       console.log(`saving user ${req.body.name} with email ${req.body.email} and request ${req.body.message}`);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const router = express.Router();
+// const express = require("express");
+// const router = express.Router();
 
-router.get('/backend/test', (req,res)=>{
-    res.send('we made it!!!!')
-});
-router.post('/user/interest', (req, res) => {
-    console.log(req.body);
-    console.log(`saving user ${req.body.name} with email ${req.body.email} and request ${req.body.message}`);
-    db.users.create({
-        name: req.body.firstname,
-        email: req.body.email,
-        message: req.body.message,
-    })
-});
+// router.get('/backend/test', (req,res)=>{
+//     res.send('we made it!!!!')
+// });
+// router.post('/user/interest', (req, res) => {
+//     console.log(req.body);
+//     console.log(`saving user ${req.body.name} with email ${req.body.email} and request ${req.body.message}`);
+//     db.users.create({
+//         name: req.body.firstname,
+//         email: req.body.email,
+//         message: req.body.message,
+//     })
+// });
 
 
-module.exports = router;
+// module.exports = router;
